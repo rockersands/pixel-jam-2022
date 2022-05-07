d0dec4ea23665a63e6ccf71454dfc133d34646b3
kurei <tamaotamamura@gmail.com>
2022-05-07 01:51:30 -0500

    se agregaron los scripts para el inventario

---
+++ Pix3l Game Jam 2022/Assets/Scritps/Inventory/Inventory.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+    public Text txtCocaCount;
+    public int maxCoins;
+    public int countCoins;
+
+    public bool getCoca= false;
+    public int cocaCount;
+}
