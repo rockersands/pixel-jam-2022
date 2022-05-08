a532b53c6338e1113c6dbc507f0a8b93a39af890
rockersands <joaquinfrausto12@hotmail.com>
2022-05-08 00:39:33 -0500

    update bug

--- Pix3l Game Jam 2022/Assets/Scritps/Inventory/ObjectCollect.cs
+++ Pix3l Game Jam 2022/Assets/Scritps/Inventory/ObjectCollect.cs
@@ -9,23 +9,6 @@ public class ObjectCollect : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag.Equals(objTag))
-        {
-            FindObjectOfType<Inventory>().countCoins++; 
-            // agregar sonido de recoleccion de una moneda
-            if(FindObjectOfType<Inventory>().countCoins == FindObjectOfType<Inventory>().maxCoins)
-            {
-                FindObjectOfType<Inventory>().getCoca = true;
-            }
-        }
-        else if (collision.tag.Equals(cocaTag))
-        {
-            if (FindObjectOfType<Inventory>().getCoca)
-            {
-                FindObjectOfType<Inventory>().cocaCount++; //poner en la caja de texto
-                FindObjectOfType<Inventory>().txtCocaCount.text = "" + FindObjectOfType<Inventory>().cocaCount;
-                FindObjectOfType<Inventory>().getCoca = false;
-            }
-        }
+       
     }
 }
